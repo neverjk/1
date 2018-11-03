c5185e80bb99e00ba5815d9c6bf9b118fd004db6
Anastasia Pashynska <44616819+neverjk@users.noreply.github.com>
2018-11-03 02:15:44 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,17 @@
-#! /bin/bash
+#! bin/bash
 
-
+number=`grep -in '<script>bpoAppVersion' url`
 date=$(( `date +%H` + `date +%M` ))
 echo "$date"
+echo "$number"
+if (( "$date" > "$number" ))
+then
+        echo "$date > $number"
+elif (( "$date" < "$number" ))
+then
+        echo "$date <  $number"
+else
+        echo "$date = $number"
+fi
+
 
